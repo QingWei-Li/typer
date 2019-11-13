ebd6a390afeda50f46f3fbf05a735f8ea1c288bc
liqingwei <liqingwei@shimo.im>
2019-11-13 13:30:46 +0800

    fix: name

--- src/lambda/render.ts
+++ src/lambda/render.ts
@@ -38,7 +38,8 @@ export async function handler(event) {
     <html>
     <head>
       <meta charset="utf-8"/>
-      <title>${(title ? title.text : "Untitled") + " - Powered by Wen"}</title>
+      <title>${(title ? title.text : "Untitled") +
+        " - Powered by Typer"}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <link href="https://fonts.googleapis.com/css?family=Roboto:400,500|Roboto+Mono" rel="stylesheet">
